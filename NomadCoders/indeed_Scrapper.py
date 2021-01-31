e57b6c9457a795603e4921eece1550dd8915d3ea
rachel5004 <75432228+rachel5004@users.noreply.github.com>
2021-01-31 15:55:45 +0900

    indeed_Scrapper

---
+++ NomadCoders/indeed_Scrapper.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+
+limit = 50
+url = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=any&limit={limit}&sort=&psf=advsrch&from=advancedsearch"
+
+
+def extract_indeed_pages():
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html.parser")
+
+    pagination = soup.select_one("ul.pagination-list")
+    pages = pagination.select('li > a > span')
+
+    spans = []
+    for page in pages[:-1]:
+        spans.append(int(page.get_text()))
+    max_page = spans[-1]
+    return max_page
+
+
+def extract_indeed_jobs(last_page):
+    jobs = []
+    for n in range(last_page):
+        print(f"Scrapping page {n}...")
+        result = requests.get(f"{url}&start={n * limit}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div",{"class":"jobsearch-SerpJobCard"})
+        #print(results)
+        for result in results:
+            job = extract_jobs(result)
+            jobs.append(job)
+    return jobs
+
+
+def extract_jobs(html):
+    title = html.find("h2", {"class": "title"})
+    title_anchor = title.find("a")["title"]
+    title = title_anchor
+
+    company = html.find("span", {"class": "company"})
+    if company.find("a") is not None:
+        company = company.find("a").string.replace("\n","")
+    else:
+        company = company.string.replace("\n","")
+
+    location = html.find("div",{"class":"recJobLoc"})["data-rc-loc"]
+
+    job_id = html["data-jk"]
+
+
+    return {"title":title, "company":company, "location":location
+            ,"link":f"https://kr.indeed.com/viewjob?jk={job_id}"}
+
+
+last_indeed_page = extract_indeed_pages()
+indeed_jobs = extract_indeed_jobs(last_indeed_page)
+
+print(indeed_jobs)
