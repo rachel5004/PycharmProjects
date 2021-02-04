b372f34eaf4b2ea1b2a6a77914e834d5b5a6d8bb
rachel5004 <75432228+rachel5004@users.noreply.github.com>
2021-02-04 21:11:36 +0900

    add save to CVS

--- NomadCoders/flask_project/test_fsk.py
+++ NomadCoders/flask_project/test_fsk.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from Scrapper import get_so_jobs
+from export import save_to_file
 app = Flask(__name__)
 db = {}
 # naver.com/reddit
@@ -33,3 +34,18 @@ def report():
                            searching_by = word,
                            resultNum = len(jobs),
                            jobs = jobs)
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file('jobs.csv')
+    except: return redirect("/")
+
