042d8dd207e7da8e80429e27e2431a5642d74165
rachel5004 <75432228+rachel5004@users.noreply.github.com>
2021-02-11 11:50:04 +0900

    Merge Sort

--- project/python/SortAlgorithm.py
+++ project/python/SortAlgorithm.py
@@ -37,8 +37,27 @@ def InsertionSort(arr):
                 arr[j], arr[j - 1] = arr[j - 1], arr[j]
     return arr
 
+def mergeSort(arr):
+    if len(arr)<2: return arr
+    mid = len(arr)//2
+    down_arr = mergeSort(arr[:mid])
+    up_arr = mergeSort(arr[mid:])
+
+    merged_Arr = []
+    d = u = 0
+    while d<len(down_arr) and u<len(up_arr):
+        if down_arr[d] < up_arr[u]:
+            merged_Arr.append(down_arr[d])
+            d+=1
+        else:
+            merged_Arr.append(up_arr[u])
+            u+=1
+    merged_Arr += down_arr[d:]
+    merged_Arr += up_arr[u:]
+    return merged_Arr
 
 num = [1,10,5,8,7,6,4,3,2,9]
 # print(BubbleSort(num))
 # print(QuickSort(num,0,len(num)-1))
-print(InsertionSort(num))
+# print(InsertionSort(num))
+print(mergeSort(num))
