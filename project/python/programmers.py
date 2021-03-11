e77465056a5912627e0c431a2863b611fec889da
rachel5004 <75432228+rachel5004@users.noreply.github.com>
2021-03-11 22:12:29 +0900

    위장

--- project/python/programmers.py
+++ project/python/programmers.py
@@ -1,4 +1,5 @@
-def solution(participant, completion):
+# hash1
+def hash1(participant, completion):
     participant.sort()
     completion.sort()
     for i in range(len(completion)):
@@ -7,7 +8,7 @@ def solution(participant, completion):
     return participant[-1]
 
 
-def solution2(participant, completion):
+def hash1_1(participant, completion):
     participant.sort()
     completion.sort()
     for p,c in zip(participant, completion):
@@ -18,17 +19,28 @@ participant = ["mislav", "stanko", "mislav", "ana"]
 completion = ["stanko", "ana", "mislav"]
 
 #2
-def solution3(phone_book):
+def hash2(phone_book):
     phone_book.sort()
     for i in range(len(phone_book) - 1):
         if phone_book[i] in phone_book[i + 1]:
             return False
     return True
-def solution4(phone_book):
+def hash2_1(phone_book):
     phone_book.sort()
     for p1, p2 in zip(phone_book, phone_book[1:]):
         if p2.startswith(p1):
             return False
     return True
 p = ["12","123","1235","567","88"]
-print(solution3(p))
+
+#3
+from collections import Counter
+def hash3(clothes):
+    c = Counter([a for _, a in clothes])
+    cnt = 1
+    for key in c:
+        cnt *= (c[key] + 1)
+    return cnt - 1
+c1=[["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
+c2 = [["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]
+print(hash3(c1))
